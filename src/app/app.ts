2efbc1a1ecffaaccf2b47165fad688f17ec3559e
delima-justine <delimajustine24@gmail.com>
2025-09-27 19:47:42 +0800

    update app component.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my_app');
+  myClass = signal('my-class');
+  myClasses = signal(['first', 'second']);
+  isActive = signal(true);
 }
