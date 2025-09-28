42cbe4068433cdbbe3a33660dc9f0a54ae2789e3
delima-justine <delimajustine24@gmail.com>
2025-09-28 13:48:12 +0800

    add structural directives

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,13 @@ import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
+type Subject = {
+    id: number;
+    subject_code: string;
+    subject_name: string;
+    no_of_units: number;
+}
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CommonModule, FormsModule],
@@ -10,10 +17,34 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+  // Class Properties
   protected readonly title = signal('my_app');
   myClass = signal('my-class');
   myClasses = signal(['first', 'second']);
   isActive = signal(false);
   fruit = signal('');
-  val = signal('')
+  day = signal(2);
+
+  subjects: Subject[] = [
+    {
+      id: 1,
+      subject_code: "COMP009",
+      subject_name: "Advance Programming",
+      no_of_units: 5
+    },
+
+    {
+      id: 2,
+      subject_code: "COMP010",
+      subject_name: "Information Management",
+      no_of_units: 5
+    },
+
+    {
+      id: 3,
+      subject_code: "COMP012",
+      subject_name: "Integrative Programming",
+      no_of_units: 3
+    }
+  ];
 }
